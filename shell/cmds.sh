b3eef9062d5dd92a15e0a0eb9d123eebd7442108
huangqingwei <huangqingwei@baidu.com>
2015-11-18 13:48:44 +0800

    add urldecode

--- shell/cmds.sh
+++ shell/cmds.sh
@@ -7,3 +7,5 @@ lftp ftp://username:password@host:port
 ./redis-cli -h 127.0.0.1 -n 1 keys 'oh:1:payment:7d742f3be61c4c8a84f6fd05928c7d014d4bed9e:*'|awk '{cmd="./redis-cli -h 127.0.0.1 -n 1 del \"" $0 "\""; system(cmd);}'
 #批量刷新(setex语法：setex key, expireTime, value)
 cat ~/datas | awk '{cmd="./redis-cli -h 127.0.0.1 -p 6379 -n 1 setex "$0" 60 4"; system(cmd);}'
+# urldecode
+perl -p -e 's/%(..)/pack("c", hex($1))/eg'
